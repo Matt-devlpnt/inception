de19f9b3848f7528d2c2ae9308f77c589e036b12
dev <dev@inception.myguest.virtualbox.org>
2024-06-10 11:10:58 +0200

    Finish version

--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-sed -i "s/\${MYSQL_DATABASE_NAME}/$MYSQL_DATABASE_NAME/g" /sites/mcordes.42.fr/wp-config.php
-sed -i "s/\${MYSQL_USER}/$MYSQL_USER/g" /sites/mcordes.42.fr/wp-config.php
-sed -i "s/\${MYSQL_PASSWORD}/$MYSQL_PASSWORD/g" /sites/mcordes.42.fr/wp-config.php
-sed -i "s/\${DOMAIN_NAME}/$DOMAIN_NAME/g" /sites/mcordes.42.fr/wp-config.php
-sed -i "s/\${AUTH_KEY}/$AUTH_KEY/g" /sites/mcordes.42.fr/wp-config.php
-sed -i "s/\${SECURE_AUTH_KEY}/$SECURE_AUTH_KEY/g" /sites/mcordes.42.fr/wp-config.php
-sed -i "s/\${LOGGED_IN_KEY}/$LOGGED_IN_KEY/g" /sites/mcordes.42.fr/wp-config.php
-sed -i "s/\${NONCE_KEY}/$NONCE_KEY/g" /sites/mcordes.42.fr/wp-config.php
-sed -i "s/\${AUTH_SALT}/$AUTH_SALT/g" /sites/mcordes.42.fr/wp-config.php
-sed -i "s/\${SECURE_AUTH_SALT}/$SECURE_AUTH_SALT/g" /sites/mcordes.42.fr/wp-config.php
-sed -i "s/\${LOGGED_IN_SALT}/$LOGGED_IN_SALT/g" /sites/mcordes.42.fr/wp-config.php
-sed -i "s/\${NONCE_SALT}/$NONCE_SALT/g" /sites/mcordes.42.fr/wp-config.php
+sleep 10
+
+php82 /usr/local/bin/wp config create --allow-root --dbname="$MYSQL_DATABASE_NAME" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb:3306 --path="/sites/$DOMAIN_NAME"
+
+php82 /usr/local/bin/wp core install --url=$DOMAIN_NAME --title='My website' --admin_user='root' --admin_password=$MYSQL_ROOT_PASSWORD --admin_email="root@$DOMAIN_NAME" --path="/sites/$DOMAIN_NAME"
+
+php82 /usr/local/bin/wp user create $MYSQL_USER $MYSQL_USER@$DOMAIN_NAME --user_pass=$MYSQL_PASSWORD --role=author --path="/sites/$DOMAIN_NAME"
 
 php-fpm82 -F
