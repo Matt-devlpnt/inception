040a9db8a5e9ddbf8557350b352d0ac0512453ac
dev <dev@inception.myguest.virtualbox.org>
2024-06-10 11:15:25 +0200

    Finish

--- monTheme/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function my_theme_enqueue_styles() {
-    wp_enqueue_style('my-theme-styles', get_template_directory_uri() . '/assets/css/styles.css');
-}
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
-?>
